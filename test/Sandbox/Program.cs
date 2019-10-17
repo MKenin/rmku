4dc43dc930075499b0cd59719d99e536b3c959ae
Encarmine <angelsanguine@gmail.com>
2019-10-17 13:05:55 +0300

    [chore] Switch library to netstandard2.1
    This project type works properly with net core 3.0 release

--- test/Sandbox/Program.cs
+++ test/Sandbox/Program.cs
@@ -1,10 +1,4 @@
 using System;
-using System.Buffers;
-using System.Buffers.Binary;
-using System.IO.Pipelines;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using Amp;
 
